525c1316f6c74c34b1dac6e05a546e83c7b614ab
00kamix00 <kamilkalemba6@gmail.com>
2026-09-03 18:58:09 +0200

    feat: fetch weather data dynamically based on user input

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import "./styles.css"
 
 
-getApi('london')
-
 async function getApi(city) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=6MD7CBSSL88JZG5HV4LB43HTD`)
         const weatherData = await response.json()
 
-        const cityName = weatherData.address
-        console.log(cityName)
+        // const cityName = weatherData.address
+        // console.log(cityName)
 
         const temperature = weatherData.days[0].temp
         console.log(temperature)
 
-        const conditions = weatherData.days[1].conditions
-        console.log(conditions)
+        // const conditions = weatherData.days[1].conditions
+        // console.log(conditions)
 
-        const humidity = weatherData.days[0].humidity
-        console.log(humidity)
+        // const humidity = weatherData.days[0].humidity
+        // console.log(humidity)
 
     } catch (error) {
         console.error(error)
     }
 }
+
+
+function renderInfo() {
+    const userCity = document.getElementById('cityName').value
+    getApi(userCity)    
+}
+
+const form = document.getElementById('weatherForm')
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    renderInfo()
+})
