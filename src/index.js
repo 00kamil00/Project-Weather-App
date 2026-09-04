fb9a8bfbd5852c338078737defc7f5310cd83ae6
00kamix00 <kamilkalemba6@gmail.com>
2026-09-04 16:42:03 +0200

    style: center card content with flexbox and refine typography

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ async function getApi(city) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=6MD7CBSSL88JZG5HV4LB43HTD`)
         const weatherData = await response.json()
 
+        const cityNameContainer = document.getElementById("city")
+        const temperatureContainer = document.getElementById("temperature")
+        const conditionsContainer = document.getElementById("conditions")
+        const humidityContainer = document.getElementById("humidity")
+
+
         const cityName = weatherData.address
+        cityNameContainer.textContent = cityName
         // console.log(cityName)
 
         const temperature = weatherData.days[0].temp
-        console.log(temperature)
+        temperatureContainer.textContent = `${temperature}°F`
+        // console.log(temperature)
 
         const conditions = weatherData.days[1].conditions
+        conditionsContainer.textContent = conditions
         // console.log(conditions)
 
         const humidity = weatherData.days[0].humidity
+        humidityContainer.textContent = `${humidity}%`
         // console.log(humidity)
 
     } catch (error) {
