d7180178fb4c1b03610b086a1df7364d9d7351a7
00kamix00 <kamilkalemba6@gmail.com>
2026-09-04 17:17:54 +0200

    feat: add temperature unit toggle buttons for Celsius and Fahrenheit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ async function getApi(city) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=6MD7CBSSL88JZG5HV4LB43HTD`)
         const weatherData = await response.json()
 
-        const cityNameContainer = document.getElementById("city")
-        const temperatureContainer = document.getElementById("temperature")
-        const conditionsContainer = document.getElementById("conditions")
-        const humidityContainer = document.getElementById("humidity")
+        const cityNameContainer = document.getElementById('city')
+        const temperatureContainer = document.getElementById('temperature')
+        const conditionsContainer = document.getElementById('conditions')
+        const humidityContainer = document.getElementById('humidity')
+        const changeCBtn = document.getElementById('change-C-btn')
+        const changeFBtn = document.getElementById('change-F-btn')
 
 
         const cityName = weatherData.address
@@ -27,6 +29,18 @@ async function getApi(city) {
         const humidity = weatherData.days[0].humidity
         humidityContainer.textContent = `${humidity}%`
         // console.log(humidity)
+
+        const tempInC = ((temperature - 32) * (5 / 9)).toFixed(1)
+        const tempInF = ((tempInC * 1.8) + 32).toFixed(1)
+
+        changeCBtn.addEventListener("click", () => {
+            temperatureContainer.textContent = `${tempInC}°C`
+        })
+
+        changeFBtn.addEventListener("click", () => {
+            temperatureContainer.textContent = `${tempInF}°F`
+        })
+
 
     } catch (error) {
         console.error(error)
