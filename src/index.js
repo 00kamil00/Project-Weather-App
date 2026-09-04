285e27c7188e404f3e588955ae0014acd0d1256a
00kamix00 <kamilkalemba6@gmail.com>
2026-09-04 16:31:08 +0200

    style: set up CSS grid layout for weather card container

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ async function getApi(city) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=6MD7CBSSL88JZG5HV4LB43HTD`)
         const weatherData = await response.json()
 
-        // const cityName = weatherData.address
+        const cityName = weatherData.address
         // console.log(cityName)
 
         const temperature = weatherData.days[0].temp
         console.log(temperature)
 
-        // const conditions = weatherData.days[1].conditions
+        const conditions = weatherData.days[1].conditions
         // console.log(conditions)
 
-        // const humidity = weatherData.days[0].humidity
+        const humidity = weatherData.days[0].humidity
         // console.log(humidity)
 
     } catch (error) {
